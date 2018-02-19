7f6f14c767819414b87124f83403055a285876f7
Singh, Bhanu Pratap <BhanuPratap_Singh@intuit.com>
2018-02-20 00:07:33 +0530

    adding code

--- src/main/java/com/bhanu/excel/tutorial/ReadDataFromExcelSheet.java
+++ src/main/java/com/bhanu/excel/tutorial/ReadDataFromExcelSheet.java
@@ -2,10 +2,13 @@ package com.bhanu.excel.tutorial;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -14,7 +17,6 @@ public class ReadDataFromExcelSheet {
 	public String[][] getExcelData(String excellocation, String sheetName) {
 		try {
 			String dataSets[][] = null;
-			
 			FileInputStream file = new FileInputStream(new File(excellocation));
 
 			// Create Workbook instance holding reference to .xlsx file
@@ -30,20 +32,17 @@ public class ReadDataFromExcelSheet {
 			dataSets = new String[totalRow][totalColumn];
 			// Iterate through each rows one by one
 			Iterator<Row> rowIterator = sheet.iterator();
-
 			int i = 0;
-			
 			while (rowIterator.hasNext()) {
 				 i = i++;
 				Row row = rowIterator.next();
-               
 					// For each row, iterate through all the columns
 					Iterator<Cell> cellIterator = row.cellIterator();
 					int j = 0;
 					while (cellIterator.hasNext()) {
 
 						Cell cell = cellIterator.next();
-						if(cell.getStringCellValue().contains("SL NO")){
+						if(cell.getStringCellValue().contains("email")){
 							 break;
 						}
 						// Check the cell type and format accordingly
@@ -67,9 +66,7 @@ public class ReadDataFromExcelSheet {
 						}
 					}
 					System.out.println("");
-				
 			}
-
 			file.close();
 			return dataSets;
 		} catch (Exception e) {
@@ -78,11 +75,47 @@ public class ReadDataFromExcelSheet {
 		return null;
 	}
 	
-	public static void main(String[] args) {
-		String excellocation = "/Users/bsingh5/git/excel-tutorial-with-dataDrivenFramework/demo.xlsx";
-		String sheetName = "TestData";
-		ReadDataFromExcelSheet excel = new ReadDataFromExcelSheet();
-		excel.getExcelData(excellocation, sheetName);
+	
+	public  void updateResult(String excellocation, String sheetName, String testCaseName, String testStatus)
+			throws IOException {
 
+		try {
+			FileInputStream file = new FileInputStream(new File(excellocation));
+
+			// Create Workbook instance holding reference to .xlsx file
+			XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+			// Get first/desired sheet from the workbook
+			XSSFSheet sheet = workbook.getSheet(sheetName);
+			// count number of active tows
+			int totalRow = sheet.getLastRowNum() + 1;
+			// count number of active columns in row
+			for (int i = 1; i < totalRow; i++) {
+				XSSFRow r = sheet.getRow(i);
+				String ce = r.getCell(1).getStringCellValue();
+				if (ce.contains(testCaseName)) {
+					r.createCell(2).setCellValue(testStatus);
+					file.close();
+					System.out.println("done");
+					FileOutputStream outFile = new FileOutputStream(new File(excellocation));
+					workbook.write(outFile);
+					outFile.close();
+					break;
+				}
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		String excellocation = "/Users/bsingh5/git/excel-tutorial-with-dataDrivenFramework/demo.xlsx";
+		String sheetName = "TestReport";
+		ReadDataFromExcelSheet excel = new ReadDataFromExcelSheet();
+		//excel.getExcelData(excellocation, sheetName);
+		excel.updateResult(excellocation, sheetName, "Login Test", "FAIL");
+		excel.updateResult(excellocation, sheetName, "Registartion Test", "PASS");
+		excel.updateResult(excellocation, sheetName, "Dashboard Test", "FAIL");
 	}
 }
