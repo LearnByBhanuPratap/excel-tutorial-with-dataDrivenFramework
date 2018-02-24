77261263e80b60f6b0d0e118f1cb91837f6e1544
Singh, Bhanu Pratap <BhanuPratap_Singh@intuit.com>
2018-02-25 00:32:49 +0530

    add

---
+++ src/test/java/com/bhanu/testScripts/login/LoginTest.java
@@ -0,0 +1,41 @@
+package com.bhanu.testScripts.login;
+
+import org.testng.SkipException;
+import org.testng.TestNGException;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.bhanu.pageObjects.home.Home;
+import com.bhanu.pageObjects.login.Login;
+import com.bhanu.testBase.DataSource;
+import com.bhanu.testBase.TestBase;
+
+public class LoginTest extends TestBase{
+	
+	Login login;
+	Home home;
+	
+	@DataProvider(name="testData")
+	public Object[][] testData(){
+		String[][] data = getExcelData("demo.xlsx", "login");
+		return data;
+	}
+	
+	@BeforeClass
+	public void beforeClass(){
+		getApplicationURL(DataSource.OR.getProperty("url"));
+		login = new Login(driver);
+	}
+	
+	
+	@Test(dataProvider="testData")
+	public void loginTest(String userName, String password, String runMode){
+		if(runMode.equalsIgnoreCase("n")){
+			throw new SkipException("Run mode for this set of data is marked N");
+		}
+		Home homePage = login.loginToApplication(userName, password);
+		homePage.logout();
+	}
+
+}
