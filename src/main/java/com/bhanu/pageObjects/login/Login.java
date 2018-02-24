6acec723118ebb58dd12aee6559bdee31511ca2d
Singh, Bhanu Pratap <BhanuPratap_Singh@intuit.com>
2018-02-24 17:49:07 +0530

    add code

--- src/main/java/com/bhanu/pageObjects/login/Login.java
+++ src/main/java/com/bhanu/pageObjects/login/Login.java
@@ -4,22 +4,43 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import com.bhanu.helper.logger.LoggerHelper;
+import com.bhanu.pageObjects.home.Home;
 
 public class Login {
 
-	public WebDriver driver;
-	
+	private WebDriver driver;
+
 	private static Logger log = LoggerHelper.getLogger(Login.class);
-	
-	
-	@FindBy(xpath="//input[@name='user_name']")
+
+	@FindBy(xpath = "//input[@name='user_name']")
 	WebElement userName;
-	
-	
-	public void setUserName(String userName){
-		log.info("entering user Name: "+userName);
+
+	@FindBy(xpath = "//input[@name='user_password']")
+	WebElement password;
+
+	public Login(WebDriver driver) {
+		super();
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+
+	public void setUserName(String userName) {
+		log.info("entering user Name: " + userName);
 		this.userName.sendKeys(userName);
+	}
+
+	public void setPassword(String password) {
+		log.info("entering password: " + password);
+		this.password.sendKeys(password);
+	}
+
+	public Home loginToApplication(String userName, String password) {
+		setUserName(userName);
+		setPassword(password);
+		return new Home(driver);
 	}
 }
--- src/main/java/com/bhanu/testBase/TestBase.java
+++ src/main/java/com/bhanu/testBase/TestBase.java
@@ -1,15 +1,60 @@
 package com.bhanu.testBase;
 
-import org.apache.log4j.Logger;
+import java.io.IOException;
 
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.bhanu.excel.tutorial.ReadDataFromExcelSheet;
 import com.bhanu.helper.logger.LoggerHelper;
 
 public class TestBase {
 	
+	public WebDriver driver;
+
 	public static Logger log = LoggerHelper.getLogger(TestBase.class);
-	
-	public static void main(String[] args) {
-		log.info("test log");
+
+	/**
+	 * This method will give excel data in 2D array based on sheet Name
+	 * @param excellocation
+	 * @param sheetName
+	 * @return
+	 */
+	public String[][] getExcelData(String excellocation, String sheetName) {
+		ReadDataFromExcelSheet readDataFromExcelSheet = new ReadDataFromExcelSheet();
+		return readDataFromExcelSheet.getExcelData(excellocation, sheetName);
 	}
 
+	/**
+	 * This method will update result in excel sheet.
+	 * @param excellocation
+	 * @param sheetName
+	 * @param testCaseName
+	 * @param testStatus
+	 * @throws IOException
+	 */
+	public void updateResult(String excellocation, String sheetName, String testCaseName, String testStatus)
+			throws IOException {
+		ReadDataFromExcelSheet readDataFromExcelSheet = new ReadDataFromExcelSheet();
+		readDataFromExcelSheet.updateResult(excellocation, sheetName, testCaseName, testStatus);
+	}
+	
+	/**
+	 * This method will create browser Objects.
+	 * @param browser
+	 * @return
+	 */
+	public WebDriver getBrowser(String browser){
+		if(browser.equalsIgnoreCase("chrome")){
+			ChromeDriver driver = new ChromeDriver();
+			return driver;
+		}
+		else if(browser.equalsIgnoreCase("firefox")){
+			FirefoxDriver driver = new FirefoxDriver();
+			return driver;
+		}
+		return null;
+	}
 }
