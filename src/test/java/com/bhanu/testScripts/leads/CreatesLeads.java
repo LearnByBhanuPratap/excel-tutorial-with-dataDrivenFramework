bb784a227d89138d171ec5dd7de20581fae50621
Singh, Bhanu Pratap <BhanuPratap_Singh@intuit.com>
2018-03-03 22:39:50 +0530

    add

---
+++ src/test/java/com/bhanu/testScripts/leads/CreatesLeads.java
@@ -0,0 +1,39 @@
+package com.bhanu.testScripts.leads;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.bhanu.pageObjects.Leads.Leads;
+import com.bhanu.pageObjects.home.Home;
+import com.bhanu.pageObjects.login.Login;
+import com.bhanu.testBase.DataSource;
+import com.bhanu.testBase.TestBase;
+
+public class CreatesLeads extends TestBase {
+
+	Login login;
+	Home home;
+
+	@DataProvider(name = "testData")
+	public Object[][] testData() {
+		String[][] data = getExcelData("demo.xlsx", "leads");
+		return data;
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+		getApplicationURL(DataSource.OR.getProperty("url"));
+		login = new Login(driver);
+	}
+
+	@Test
+	public void testCreateLead(String userName, String password, String type, String firstName, String lastName, String company, String designation, String leadSource, String industry, String annualrevenue, String noofemployees, String street,
+			String postalCode, String country, String description, String phone, String mobile, String email, String leadStatus, String runMode) {
+		
+		login.loginToApplication(userName, password);
+		Leads leads = home.navigateToLeads();
+		leads.createLeads(type, firstName, lastName, company, designation, leadSource, industry, annualrevenue, noofemployees, street, postalCode, country, description, phone, mobile, email, leadStatus);
+	}
+
+}
